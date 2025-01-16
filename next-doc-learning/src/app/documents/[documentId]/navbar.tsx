e14dd43a9be02d432fc1d24abe17d7ff2301d1bb
hoangminhduc1994 <151418180+hoangminhduc1994@users.noreply.github.com>
2025-01-16 21:10:36 +0700

    update-document-info-last

--- next-doc-learning/src/app/documents/[documentId]/navbar.tsx
+++ next-doc-learning/src/app/documents/[documentId]/navbar.tsx
@@ -21,13 +21,32 @@ import { useEditorStore } from "@/store/use-editor-store";
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 import { Inbox } from "./inbox";
 import { Doc } from "../../../../convex/_generated/dataModel";
+import { useMutation } from "convex/react";
+import { api } from "../../../../convex/_generated/api";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import { RenameDialog } from "@/components/rename-dialog";
+import { RemoveDialog } from "@/components/remove-dialog";
 
 interface NavbarProps {
     data: Doc<"documents">
 }
 
 export const Navbar = ({ data }: NavbarProps) => {
+    const router = useRouter();
     const { editor } = useEditorStore();
+    const mutation = useMutation(api.documents.create);
+    const onNewDocument = () => {
+        mutation({
+            title: "Untitled document",
+            initialContent: "",
+        })
+        .catch(() => toast.error("Something went wrong"))
+        .then((id) => {
+            router.push(`/documents/${id}`)
+            toast.success("Document created")
+        })
+    }
     const insertTable = ({rows, cols} : { rows: number, cols: number}) => {
         editor
         ?.chain()
@@ -112,19 +131,27 @@ export const Navbar = ({ data }: NavbarProps) => {
                                         </MenubarItem>
                                     </MenubarSubContent>
                                 </MenubarSub>
-                                <MenubarItem>
+                                <MenubarItem onClick={onNewDocument}>
                                     <FilePlusIcon className="size-4 mr-2"/>
                                     New Document
                                 </MenubarItem>
                                 <MenubarSeparator/>
-                                <MenubarItem>
+                                <RenameDialog documentId={data._id} initialTitle={data.title}>
+                                <MenubarItem onClick={(e) => e.stopPropagation()}
+                                onSelect={(e) => e.preventDefault()}>
                                     <FilePenIcon className="size-4 mr-2"/>
                                     Rename
                                 </MenubarItem>
-                                <MenubarItem>
+                                </RenameDialog>
+                                <RemoveDialog documentId={data._id}>
+                                <MenubarItem
+                                onClick={(e) => e.stopPropagation()}
+                                onSelect={(e) => e.preventDefault()}
+                                >
                                     <TrashIcon className="size-4 mr-2"/>
                                     Remove
                                 </MenubarItem>
+                                </RemoveDialog>
                                 <MenubarSeparator/>
                                 <MenubarItem onClick={() => window.print()}>
                                     <PrinterIcon className="size-4 mr-2"/>
--- next-doc-learning/src/components/remove-dialog.tsx
+++ next-doc-learning/src/components/remove-dialog.tsx
@@ -16,6 +16,7 @@ import { Id } from "../../convex/_generated/dataModel";
 import React, { useState } from "react";
 import { useMutation } from "convex/react";
 import { api } from "../../convex/_generated/api";
+import { useRouter } from "next/navigation";
 
 interface RemoveDialogProps {
     documentId: Id<"documents">;
@@ -23,6 +24,7 @@ interface RemoveDialogProps {
 }
 
 export const RemoveDialog = ({documentId, children}: RemoveDialogProps) => {
+    const router = useRouter();
     const remove = useMutation(api.documents.removeById);
     const [isRemoving, setIsRemoving] = useState(false);
 
@@ -49,7 +51,10 @@ export const RemoveDialog = ({documentId, children}: RemoveDialogProps) => {
                         setIsRemoving(true);
                         remove({id: documentId})
                         .catch(() => toast.error("Something went wrong"))
-                        .then(() => toast.success("Document removed"))
+                        .then(() => {
+                            toast.success("Document removed")
+                            router.push("/")
+                        })
                         .finally(() => setIsRemoving(false));
                     }}
                     >Delete</AlertDialogAction>
